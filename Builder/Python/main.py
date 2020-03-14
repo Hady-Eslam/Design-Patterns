ec9adb8c445bb8681cfbfb275509244338b0222d
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2020-03-14 21:53:00 +0200

    Create main.py

---
+++ Builder/Python/main.py
@@ -0,0 +1,14 @@
+from Builder import Home
+
+home1 = Home()
+home2 = Home()
+
+home1.BuildGarden()
+home1.BuildStatues()
+
+home2.BuildSwimPool()
+home2.BuildGarage()
+home2.BuildGarden()
+
+print(home1)
+print(home2)
