e7bf9a2b3e38f5f962db5935775e00c901aa3852
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2020-03-14 21:07:49 +0200

    Create Rooms.py

---
+++ FactoryMethod/Python/Rooms.py
@@ -0,0 +1,12 @@
+class Room:
+    pass
+
+class MagicRoom(Room):
+
+    def __init__(self):
+        print('Magic Room')
+
+class OrdinaryRoom(Room):
+
+    def __init__(self):
+        print('Ordinary Room')
