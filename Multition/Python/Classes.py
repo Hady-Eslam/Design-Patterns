75403b8f09838588d4df65dde68dedb0e0720ef9
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2020-03-15 01:20:14 +0200

    Create Classes.py

---
+++ Multition/Python/Classes.py
@@ -0,0 +1,14 @@
+class Class1:
+
+    def __init__(self):
+        print('Hello From Class1')
+
+class Class2:
+
+    def __init__(self):
+        print('Hello From Class2')
+
+class Class3:
+
+    def __init__(self):
+        print('Hello From Class3')
