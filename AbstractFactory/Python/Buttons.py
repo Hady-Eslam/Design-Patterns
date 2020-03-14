02616591b91041af11f9968bab1dd01f49bc5088
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2020-03-14 20:16:25 +0200

    Create Buttons.py

---
+++ AbstractFactory/Python/Buttons.py
@@ -0,0 +1,17 @@
+class Button:
+    pass
+
+class RedButton(Button):
+
+    def __init__(self):
+        print('Red Button')
+
+class GreenButton(Button):
+
+    def __init__(self):
+        print('Green Button')
+
+class BlueButton(Button):
+
+    def __init__(self):
+        print('Blue Button')
