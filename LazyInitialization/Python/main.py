a2f740dc6feb637f45ef3e8c0e401514a8bac382
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2020-03-15 13:58:36 +0200

    Create main.py

---
+++ LazyInitialization/Python/main.py
@@ -0,0 +1,15 @@
+from Fruits import Fruits
+
+fruits = Fruits()
+
+fruits.GetFruit('Apple')
+fruits.PrintFruits()
+
+fruits.GetFruit('Banana')
+fruits.PrintFruits()
+
+fruits.GetFruit('Apple')
+fruits.PrintFruits()
+
+fruits.GetFruit('WaterMelon')
+fruits.PrintFruits()
