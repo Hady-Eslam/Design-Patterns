cfa98b796e645d7a92ed637f4d0d0a40c014acf0
Hady Eslam <30216972+Hady-Eslam@users.noreply.github.com>
2020-03-16 18:35:48 +0200

    Create Bird.py

---
+++ Adapter/Python/Bird.py
@@ -0,0 +1,15 @@
+class Bird:
+
+    def Fly(self):
+        pass
+    
+    def MakeSound(self):
+        pass
+
+class Sparrow(Bird):
+
+    def Fly(self):
+        print('I Am Flying')
+    
+    def MakeSound(self):
+        print('Chrip Chrip')
